edcf9c5e2f4477ff4f480c8af70a9860cc097a25
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-06-27 22:49:54 +0300

    fixing country container link behaviour

--- src/components/country/country.component.jsx
+++ src/components/country/country.component.jsx
@@ -1,13 +1,12 @@
 import {useContext} from 'react';
 import { ThemeContext } from "../../context/theme.context";
-import {CountryLink,CountryImage,InfoContainer,CountryItem} from './country.styles.jsx';
+import {CountryImage,InfoContainer,CountryItem} from './country.styles.jsx';
 
 function Country(props){
     const {theme}=useContext(ThemeContext);
     const {flag,name,population,region,capital,alpha3Code}=props;
     return(
-        <CountryItem dark={theme}>
-            <CountryLink to={`/${alpha3Code}`}>
+        <CountryItem dark={theme} to={`/${alpha3Code}`}>
                 <CountryImage data-alpha2code={alpha3Code} src={flag} alt={name}/>
                 <InfoContainer>
                     <p className="countryName">{name}</p>
@@ -15,7 +14,6 @@ function Country(props){
                     <p className="countryInfo">{"Region:"+region}</p>
                     <p className="countryInfo">{"Capital:"+capital}</p>
                 </InfoContainer>
-            </CountryLink>
         </CountryItem>
     );
 }
