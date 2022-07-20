96087b5cad95c7ea8d6b21cd3fff962c3b85d31e
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-07-20 19:01:02 +0300

    adding route for 404 page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import {Routes,Route} from 'react-router-dom';
 import Home from './routes/home/home.component.jsx';
 import CountryPage from './routes/countryPage/countryPage.component.jsx';
+import ErrorPage from './routes/error/error.component.jsx';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/:countryCode' element={<CountryPage/>}/>
+      <Route path='*' element={<ErrorPage/>}/>
     </Routes>
   );
 }
